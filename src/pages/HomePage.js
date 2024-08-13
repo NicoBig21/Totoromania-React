f091d41c9fd9b9623489ce8f6b2ac4bbb5957a0a
NicoBig21 <nicobignone21@gmail.com>
2024-08-13 13:32:42 -0300

    arrangements

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,7 +14,7 @@ const HomePage = (props) => {
                     <p>
                         Pero la magia de TotoroMania no se detiene en los peluches. Nuestra colección de ropa de lana tejida es una celebración de la comodidad y el estilo. Desde suéteres y bufandas hasta gorros y calcetines, cada prenda está diseñada con amor y con los materiales más suaves para mantener tu cuerpo abrigado y tu espíritu feliz. En TotoroMania, nos esforzamos por brindarte una experiencia de compra excepcional, desde la navegación en nuestro sitio web hasta la entrega de tu pedido en la puerta de tu hogar. ¡Descubre la magia de TotoroMania y encuentra tu próxima pieza favorita hoy mismo!
                     </p>
-                    <a href="nosotros.html" className="btn-custom">Nosotros</a>
+                    <a href="nosotros" className="btn-custom">Nosotros</a>
                 </div>                
                 <div className="col-md-6">
                     <img src="/images/afiche-promocional.jpg" className="img-fluid img-border" alt="Peluches"/>
@@ -27,7 +27,7 @@ const HomePage = (props) => {
                         <div className="card-body">
                             <h5 className="card-title">Ropa de lana tejida</h5>
                             <p className="card-text">Descubre nuestra colección de ropa de lana tejida, perfecta para mantenerte abrigado y con estilo en cualquier ocasión.</p>
-                            <a href="galeria.html" className="btn-custom">Ver colección</a>
+                            <a href="galeria" className="btn-custom">Ver colección</a>
                         </div>
                     </div>
                 </div>
@@ -37,7 +37,7 @@ const HomePage = (props) => {
                         <div className="card-body">
                             <h5 className="card-title">Peluches adorables</h5>
                             <p className="card-text">Explora nuestra selección de peluches de alta calidad, desde personajes clásicos hasta los más populares de hoy en día.</p>
-                            <a href="galeria.html" className="btn-custom">Ver colección</a>
+                            <a href="galeria" className="btn-custom">Ver colección</a>
                         </div>
                     </div>
                 </div>
@@ -47,7 +47,7 @@ const HomePage = (props) => {
                         <div className="card-body">
                             <h5 className="card-title">Accesorios de lana</h5>
                             <p className="card-text">Lleva contigo el estilo y la calidez con nuestra exclusiva mochila de lana. Confeccionada con los materiales más suaves y cómodos.</p>
-                            <a href="galeria.html" class="btn-custom">Ver colección</a>
+                            <a href="galeria" className="btn-custom">Ver colección</a>
                         </div>
                     </div>
                 </div>
--- src/pages/ServiciosPage.js
+++ src/pages/ServiciosPage.js
@@ -4,47 +4,47 @@ const ServiciosPage = (props) => {
     return(
     <main className="container">
         <section id="servicios" className="container">
-            <div class="row">
-                <div class="col-12 text-center mb-4">
+            <div className="row">
+                <div className="col-12 text-center mb-4">
                     <h2>Nuestros Servicios</h2>
-                    <p class="lead">En TotoroMania ofrecemos una amplia gama de servicios para satisfacer todas tus necesidades.</p>
+                    <p className="lead">En TotoroMania ofrecemos una amplia gama de servicios para satisfacer todas tus necesidades.</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-6 mb-4">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Venta de Ropa Tejida</h5>
-                            <p class="card-text">Explora nuestra colección de ropa tejida a mano, desde suéteres hasta bufandas, para mantenerte abrigado y con estilo.</p>
+            <div className="row">
+                <div className="col-md-6 mb-4">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Venta de Ropa Tejida</h5>
+                            <p className="card-text">Explora nuestra colección de ropa tejida a mano, desde suéteres hasta bufandas, para mantenerte abrigado y con estilo.</p>
                         </div>
-                        <img src="/images/ropa-tejida.jpg" class="card-img-bottom" alt="Venta de Ropa Tejida"/>
+                        <img src="/images/ropa-tejida.jpg" className="card-img-bottom" alt="Venta de Ropa Tejida"/>
                     </div>
                 </div>
-                <div class="col-md-6 mb-4">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Atención al Cliente</h5>
-                            <p class="card-text">¿Tienes algún problema con tu pedido? Nuestro equipo de atención al cliente está aquí para ayudarte con cualquier defecto o inconveniente.</p>
+                <div className="col-md-6 mb-4">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Atención al Cliente</h5>
+                            <p className="card-text">¿Tienes algún problema con tu pedido? Nuestro equipo de atención al cliente está aquí para ayudarte con cualquier defecto o inconveniente.</p>
                         </div>
-                        <img src="/images/atencion-cliente.jpg" class="card-img-bottom" alt="Atención al Cliente"/>
+                        <img src="/images/atencion-cliente.jpg" className="card-img-bottom" alt="Atención al Cliente"/>
                     </div>
                 </div>
-                <div class="col-md-6 mb-4">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Productos Personalizados</h5>
-                            <p class="card-text">¿Quieres algo único? Ofrecemos productos personalizados, desde juguetes para mascotas hasta regalos especiales hechos a medida.</p>
+                <div className="col-md-6 mb-4">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Productos Personalizados</h5>
+                            <p className="card-text">¿Quieres algo único? Ofrecemos productos personalizados, desde juguetes para mascotas hasta regalos especiales hechos a medida.</p>
                         </div>
-                        <img src="/images/juguete-personalizado.jpg" class="card-img-bottom" alt="Productos Personalizados"/>
+                        <img src="/images/juguete-personalizado.jpg" className="card-img-bottom" alt="Productos Personalizados"/>
                     </div>
                 </div>
-                <div class="col-md-6 mb-4">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Pedidos a Medida</h5>
-                            <p class="card-text">Cuéntanos tu idea y nosotros la haremos realidad. Diseñamos y creamos productos a medida según tus especificaciones.</p>
+                <div className="col-md-6 mb-4">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Pedidos a Medida</h5>
+                            <p className="card-text">Cuéntanos tu idea y nosotros la haremos realidad. Diseñamos y creamos productos a medida según tus especificaciones.</p>
                         </div>
-                        <img src="/images/medida.jpg" class="card-img-bottom" alt="Pedidos a Medida"/>
+                        <img src="/images/medida.jpg" className="card-img-bottom" alt="Pedidos a Medida"/>
                     </div>
                 </div>
             </div>
